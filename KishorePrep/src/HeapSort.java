c99a21fb3271f3741165dcde2f5a5993a080729f
KISHORE <KISHORE@LAPTOP-M0GJ097N>
2018-08-12 19:21:24 +0530

    HeapSort

--- KishorePrep/src/HeapSort.java
+++ KishorePrep/src/HeapSort.java
@@ -7,26 +7,17 @@ public class HeapSort
 	public void sort(int arr[])
 	{
 		int n = arr.length;
-
-
 		for (int i = n / 2 - 1; i >= 0; i--)
 			heapify(arr, n, i);
-
-
 		for (int i=n-1; i>=0; i--)
 		{
-
 			int temp = arr[0];
 			arr[0] = arr[i];
 			arr[i] = temp;
-
-
 			heapify(arr, i, 0);
 		}
 	}
 
-	// To heapify a subtree rooted with node i which is
-	// an index in arr[]. n is size of heap
 	void heapify(int arr[], int n, int i)
 	{
 		int largest = i; // Initialize largest as root
@@ -52,8 +43,6 @@ public class HeapSort
 			heapify(arr, n, largest);
 		}
 	}
-
-
 	static void printArray(int arr[])
 	{
 		int n = arr.length;
@@ -61,8 +50,6 @@ public class HeapSort
 			System.out.print(arr[i]+" ");
 		System.out.println();
 	}
-
-
 	public static void main(String args[])
 	{
 		int arr[] = {12, 11, 13, 5, 6, 7};
