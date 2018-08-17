5042a3d21ea4f91eb4226704c41b2b4c94236766
ravikishore malireddi <ravikishore9835@gmail.com>
2018-08-17 20:37:42 +0530

    Create balanced.java

---
+++ balanced.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+         int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            String s = in.next();
+            Stack<Integer> st =new Stack<Integer>();
+            char a[]=s.toCharArray();
+            int comp=0;
+            for(int i=0;i<s.length();i++){
+            	if(i>0 && !st.isEmpty()){comp=st.peek();}
+            	st.push((int) a[i]);
+            	if(comp!=0 && !st.isEmpty() && st.size()>1){
+            	if((comp==91 && st.peek()==93)||(comp==123 && st.peek()==125)||(comp==40 && st.peek()==41)){
+            	st.pop();st.pop();}
+            	}
+            	}
+            if(st.isEmpty()){System.out.println("YES");}
+            else{System.out.println("NO");}
+        }
+    }
+}
